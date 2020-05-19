87ed01bd6467d9ffc87ee27467536f707c614a74
alinemx <alimontexim@gmail.com>
2020-05-19 12:15:29 -0300

    Modificação do valor

--- TestaConversao/src/TestaConversao.java
+++ TestaConversao/src/TestaConversao.java
@@ -3,7 +3,7 @@ public class TestaConversao {
 	
 	public static void main (String [] args) {
 	
-		double salario = 1270.50;
+		double salario = 1288.50;
 		int valor = (int)salario;
 		System.out.println(valor);
 		
